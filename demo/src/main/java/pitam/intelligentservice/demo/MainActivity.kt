3e2a53428bc5c6eb486219f736a339d989ac9419
Pitam Poudel <pitampoudelsaipu@gmail.com>
2022-09-24 11:50:20 +0545

    app module rename + hilt added

---
+++ demo/src/main/java/pitam/intelligentservice/demo/MainActivity.kt
@@ -0,0 +1,12 @@
+package pitam.intelligentservice.demo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
